860006664383832c13b3f291847a260b6752ee80
KHAV <251593999+KhAnnVit@users.noreply.github.com>
2026-03-07 21:27:23 +0300

    Added FSM for registration

--- handlers/routes.py
+++ handlers/routes.py
@@ -1,8 +1,15 @@
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from forms.user import Form
+from aiogram.fsm.context import FSMContext
 
 router = Router()
+
+
+
+
+
 
 def get_main_reply_keyboard():
     keyboard = ReplyKeyboardMarkup(
@@ -30,7 +37,7 @@ def get_basic_reply_keyboard():
 
 
 @router.message(Command("start"))
-async def hello(message: Message):
+async def hello(message: Message, state: FSMContext):
     await message.answer(
         "hello",
     parse_mode="Markdown",
@@ -42,3 +49,37 @@ async def basic(message: Message):
         "Выбери, что хочешь сделать",
     parse_mode="Markdown",
     reply_markup=get_basic_reply_keyboard())
+
+@router.message(F.text=="Регистрация")
+async def basic(message: Message, state: FSMContext):
+    await message.answer(
+        "Давай сначала заполним анкету. Введите имя:",
+    parse_mode="Markdown")
+    await state.set_state(Form.name)
+
+@router.message(Form.name, F.text)
+async def process_name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Отлично! Введите ваш возраст")
+    await state.set_state(Form.age)
+
+
+@router.message(Form.age, F.text)
+async def process_name(message: Message, state: FSMContext):
+    if not message.text.isdigit():
+        await message.answer("Возраст должен быть числом")
+        return
+    await state.update_data(age=int(message.text))
+    await message.answer("Отлично! Введите ваш пол")
+    await state.set_state(Form.gender)
+
+
+@router.message(Form.gender, F.text)
+async def process_name(message: Message, state: FSMContext):
+    await state.update_data(gender=message.text)
+    await message.answer("Отлично! Введите ваш пол")
+    data = await state.get_data()
+    name = data["name"]
+    age = data["age"]
+    gender = data["gender"]
+    await state.clear()
